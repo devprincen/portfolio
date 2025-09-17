58d30ae8321e4a32cfc6a5dc63ad92d17bd9a205
Prince Goswami <princenath0757@gmail.com>
2025-09-17 20:01:19 +0530

    Added my porfolio html css and javascript  file

---
+++ web.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Blogger", "Student"],
+    typeSpeed: 90,
+    backSpeed: 90,
+    backDelay: 1000,
+    loop: true
+})
